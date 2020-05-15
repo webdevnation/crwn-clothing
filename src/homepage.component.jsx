3c9e99bf480e4a12b5dc765cc69899710c34a805
unknown <jdogisme@hotmail.comm>
2020-05-15 12:55:00 -0400

    Created project with create react app and added homepage files

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './homepage.styles.scss';
 
+
 const HomePage = () =>(
     <div className="homepage">
         <div className="directory-menu">
@@ -11,7 +12,7 @@ const HomePage = () =>(
                 </div>
             </div>
             <div className="menu-item">
-                <div className="content">
+                 <div className="content">
                     <h1 className="title">JACKETS</h1>
                     <span className="subtitle">SHOP NOW</span>
                 </div>
